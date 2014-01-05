689f07b24d0fb1f886d6d60dfdaa5fb90f6b84e8
Bruno Carriere <neonskimmer@gmail.com>
2014-01-05 10:28:30 -0500

    Added guard, clean up

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var express = require('express');
 var logfmt = require('logfmt');
 var app = express();
 
-app.use('/', express.static('static'))
+app.use('/', express.static('static'));
 app.use(logfmt.requestLogger());
 
 app.get('/ping', function(req, res) {
@@ -10,9 +12,13 @@ app.get('/ping', function(req, res) {
 });
 
 app.get('/primeFactors', function(req, res) {
-  var num = req.query.number || null;
-  if (null) {
-    return res.json([]);
+  var num = parseInt(req.query.number, 10);
+
+  if (Number.isNaN(num)) {
+    return res.json({
+      number: req.query.number,
+      error: 'not a number'
+    });
   }
   else {
     res.json({
@@ -26,4 +32,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
-
