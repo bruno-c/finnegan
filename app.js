604c4e8d3cae0546a45ba202fea2aa1ef66437d7
Bruno Carriere <neonskimmer@gmail.com>
2014-01-05 09:59:49 -0500

    Added static folder

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var logfmt = require('logfmt');
 var app = express();
 
+app.use('/', express.static('static'))
 app.use(logfmt.requestLogger());
 
 app.get('/ping', function(req, res) {
@@ -10,5 +11,5 @@ app.get('/ping', function(req, res) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
